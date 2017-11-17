8ef94eb8778d5418e0724c99f5dd7a82e9df22f1
Duncan Forgan <dhf3@st-andrews.ac.uk>
2017-11-17 14:45:25 +0000

    New module `io_tache.py` to reduce code repetition

---
+++ plot/io_tache.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import read_eigenvalue_file as eigen
+
+
+def read_eigenvalue_file(filename):
+    '''Calls the f2py functions needed to read the eigenvalue file'''
+
+    nelement = eigen.find_number_entries(filename)
+    x,y,z,eigenelement,eigenvalues = eigen.read_file(filename,nelement)
+
+    eigenvalues = eigenvalues.transpose()
+
+    return nelement,x,y,z,eigenelement,eigenvalues
+
+def classify_eigenvalue(eigenvalues, threshold):
+    '''Given 3 eigenvalues, and some threshold, returns an integer
+    'iclass' corresponding to the number of eigenvalues below the threshold
+    
+    iclass = 0 --> clusters (3 +ve eigenvalues, 0 -ve)
+    iclass = 1 --> filaments (2 +ve eigenvalues, 1 -ve)
+    iclass = 2 --> sheet (1 +ve eigenvalues, 2 -ve)
+    iclass = 3 --> voids (0 +ve eigenvalues, 3 -ve)
+    
+    '''
+    
+    iclass = 0
+    
+    for i in range(3):
+
+        if(eigenvalues[i]<threshold):
+            iclass +=1
+            
+    return int(iclass)
+
+def classify_all_eigenvalues(eigenvalues,nelement,threshold):
+    '''Classifies `nelement` eigenvalues using classify_eigenvalue'''
+
+    classification = np.empty(nelement,dtype='int')
+
+    for i in range(nelement):
+        eig = eigenvalues[i,:]
+        classification[i] = classify_eigenvalue(eig,threshold)
+
+
+    # Also return array indices for each class
+
+    return classification
+
+def print_class_counts(classification,filename):
+
+     # Array indices for each class
+    nclusters = np.size(classification[classification[:]==0])
+    nfilaments = np.size(classification[classification[:]==1])
+    nsheets = np.size(classification[classification[:]==2])
+    nvoids = np.size(classification[classification[:]==3])
+
+    print "File: ",filename
+    print "Clusters: ",nclusters
+    print "Filaments: ",nfilaments
+    print "Sheets: ",nsheets
+    print "Voids: ",nvoids
+
+    return nclusters,nfilaments,nsheets,nvoids
+
+
+def plot_all_classes_xy(x,y,filename,classification,threshold):
+
+    xmin = np.amin(x)
+    xmax = np.amax(x)
+
+    ymin = np.amin(y)
+    ymax = np.amax(y)
+
+    # Array indices for each class
+    index_clusters = classification[:]==0
+    index_filaments = classification[:]==1
+    index_sheets = classification[:]==2
+    index_voids = classification[:]==3
+
+    fig1, ((axcl,axfil),(axsh,axvd)) = plt.subplots(2,2, sharex='col',sharey='row')
+
+    # Decide on plot title using filename:
+    # Files containing "T" have been calculated using the tidal tensor
+    # Files containing "V" have been calculated using the velocity shear tensor
+    
+    plot_title = "Tensor Classification, Threshold ="+str(threshold)
+        
+    if "T" in filename:
+        plot_title = "Tidal Tensor Classification, Threshold ="+str(threshold)
+
+    if "V" in filename:
+        plot_title = "Velocity Shear Tensor Classification, Threshold ="+str(threshold)
+
+    plt.suptitle(plot_title)
+    axcl.set_title("Clusters")
+    axcl.hexbin(x[index_clusters],y[index_clusters],gridsize=500)
+    axcl.set_xlim(xmin,xmax)
+    axcl.set_ylim(ymin,ymax)
+
+    axfil.set_title("Filaments")
+    axfil.hexbin(x[index_filaments],y[index_filaments],gridsize=500)
+    axfil.set_xlim(xmin,xmax)
+    axfil.set_ylim(ymin,ymax)
+    
+    axsh.set_title("Sheets")
+    axsh.hexbin(x[index_sheets],y[index_sheets],gridsize=500)
+    axsh.set_xlim(xmin,xmax)
+    axsh.set_ylim(ymin,ymax)
+    
+    axvd.set_title("Voids")
+    axvd.hexbin(x[index_voids],y[index_voids],gridsize=500)
+    axvd.set_xlim(xmin,xmax)
+    axvd.set_ylim(ymin,ymax)
+    
+
+    return fig1
+
+    #outputfile = "classify_"+filename+"_"+str(threshold)+".png"
+    #print "Plotting to file ",outputfile
+         
+    #plt.savefig(outputfile,format="png")
--- plot/plot_nclass_choosethreshold.py
+++ plot/plot_nclass_choosethreshold.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import classify_eigenvalues as classify
+import io_tache as io
 import filefinder as ff
 import read_eigenvalue_file
 from sys import argv
@@ -17,13 +17,16 @@ threshold = input("What is the threshold for classification? ")
   
 # Read in eigenvalue file
 print "Reading eigenvalue file ", filename
+npart,x,y,z,eigenpart,eigenvalues = io.read_eigenvalue_file(filename)
 
-npart = read_eigenvalue_file.find_number_entries(filename)
-x,y,z,eigenpart,eigenvalues = read_eigenvalue_file.read_file(filename,npart)
+
+
+#npart = read_eigenvalue_file.find_number_entries(filename)
+#x,y,z,eigenpart,eigenvalues = read_eigenvalue_file.read_file(filename,npart)
 
 
 # fortran does rows and columns differently from python - switch them here
-eigenvalues = eigenvalues.transpose()
+#eigenvalues = eigenvalues.transpose()
 
 tryagain = 'y'
 
@@ -32,80 +35,15 @@ tryagain = 'y'
 while(tryagain!='n'):
 
     print "Classifying eigenvalues according to threshold ", threshold    
-    # Classify eigenvalues
-    # Function returns an integer iclass
-    #     iclass = 0 --> cluster
-    #     iclass = 1 --> filament
-    #     iclass = 2 --> sheet
-    #     iclass = 3 --> void
-
-    classification = np.empty(npart, dtype="int")
-
-
-    for i in range(npart):
-        eig = eigenvalues[i,:]
-    
-        classification[i] = classify.classify_eigenvalue(eig, threshold)
+ 
+    classification = io.classify_all_eigenvalues(eigenvalues,npart,threshold)
 
     # Give some statistics on the classification
 
-    icluster = classification[:]==0
-    ifilament = classification[:]==1
-    isheet = classification[:]==2
-    ivoid = classification[:]==3
+    nclusters,nfilaments,nsheets,nvoids = io.print_class_counts(classification,filename)
 
-    clusters = classification[icluster]
-    filaments = classification[ifilament]
-    sheets = classification[isheet]
-    voids = classification[ivoid]
-
-    print "Clusters: ",np.size(clusters)
-    print "Filaments: ",np.size(filaments)
-    print "Sheets: ", np.size(sheets)
-    print "Voids: ", np.size(voids)
-
-    # Now plot this data
-
-    xmin = np.amin(x)
-    xmax = np.amax(x)
-
-    ymin = np.amin(y)
-    ymax = np.amax(y)
-
-    fig1, ((axcl,axfil),(axsh,axvd)) = plt.subplots(2,2, sharex='col',sharey='row')
-
-    # Decide on plot title using filename:
-    # Files containing "T" have been calculated using the tidal tensor
-    # Files containing "V" have been calculated using the velocity shear tensor
-    
-    plot_title = "Tensor Classification, Threshold ="+str(threshold)
-    
-    if "T" in filename:
-        plot_title = "Tidal Tensor Classification, Threshold ="+str(threshold)
-    
-    if "V" in filename:
-        plot_title = "Velocity Shear Tensor Classification, Threshold ="+str(threshold)
-
-    plt.suptitle(plot_title)
-    axcl.set_title("Clusters")
-    axcl.hexbin(x[icluster],y[icluster],gridsize=500)
-    axcl.set_xlim(xmin,xmax)
-    axcl.set_ylim(ymin,ymax)
-
-    axfil.set_title("Filaments")
-    axfil.hexbin(x[ifilament],y[ifilament],gridsize=500)
-    axfil.set_xlim(xmin,xmax)
-    axfil.set_ylim(ymin,ymax)
-
-    axsh.set_title("Sheets")
-    axsh.hexbin(x[isheet],y[isheet],gridsize=500)
-    axsh.set_xlim(xmin,xmax)
-    axsh.set_ylim(ymin,ymax)
-
-    axvd.set_title("Voids")
-    axvd.hexbin(x[ivoid],y[ivoid],gridsize=500)
-    axvd.set_xlim(xmin,xmax)
-    axvd.set_ylim(ymin,ymax)
+    # Plot classifications binned in x,y
+    fig1 = io.plot_all_classes_xy(x,y,filename,classification,threshold)
 
     plt.show()
     
